63a0e3aa996a530ccfc55f6db2905bd20662d41f
unknown <andreyxdd@yandex.ru>
2022-06-07 00:55:48 -0600

    Display the plan type on the plan screen using firstore hooks

--- screens/withAuth/Plan/PlanScreen.tsx
+++ screens/withAuth/Plan/PlanScreen.tsx
@@ -1,5 +1,9 @@
 import { StyleSheet } from 'react-native';
 
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { collection } from 'firebase/firestore';
+import { db } from '../../../firebase';
+import useStore, { IStore } from '../../../hooks/useStore';
 import PopupPlanMenu from '../../../components/PopupPlanMenu';
 import { Text, View } from '../../../components/Themed';
 
@@ -21,9 +25,19 @@ const styles = StyleSheet.create({
 });
 
 export default function PlanScreen() {
+  const uid = useStore((state: IStore) => state.uid);
+  const [value, loading, error] = useCollection(
+    collection(db, 'users', uid!, 'plans'),
+    {
+      snapshotListenOptions: { includeMetadataChanges: true },
+    },
+  );
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Three - Counter</Text>
+      {value && !loading && !error
+        ? <Text style={styles.title}>{value.docs[0].data()?.type}</Text>
+        : null}
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <PopupPlanMenu />
     </View>
